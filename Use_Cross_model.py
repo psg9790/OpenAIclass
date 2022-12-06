061421cddac8ec39d51978e51b2d37f5f9a2aa0c
psg9790 <81291990+psg9790@users.noreply.github.com>
2022-12-06 11:13:14 +0900

    3. 모델 사용 예측
    모델 사용해서 예측하는 스크립트입니다

--- Use_Cross_model.py
+++ Use_Cross_model.py
@@ -1,31 +1,33 @@
+# 앞서 학습 후 저장했던 모델을 불러와서 예측에 사용해볼 것입니다
+
 import time
 import requests
 import keras
-import tensorflow as tf
 import pandas as pd
-from sklearn.model_selection import train_test_split
 import numpy as np
-import matplotlib.pyplot as plt
 
 # 파라미터
-candles_data = 15   # 사용할 분봉 데이터
+# 사용할 분봉
+candles_data = 15
+# 타깃 MA1
 target_MA_1 = 10
+# 타깃 MA2
 target_MA_2 = 20
 
 # 모델 불러오기
 model = keras.models.load_model('./Model/cross_model'+str(candles_data)+'_'+ str(target_MA_1) + '_' + str(target_MA_2) +'.h5')
 model.summary()
 
-# 타겟 시간
+# 타겟 시간 - 현재 시간을 기준으로 예측해봅니다
 targetTimeStr = time.strftime('%Y-%m-%dT%H:%M:%S', time.localtime((time.time())))
 print(targetTimeStr)
 
-# API
+# API로 모델에 입력할 이전 데이터들을 불러와야 합니다
 url = "https://api.upbit.com/v1/candles/minutes/"+str(candles_data)+"?market=KRW-BTC&to="+targetTimeStr+"%2B09:00&count=200"
 headers = {"accept": "application/json"}
 response = requests.get(url, headers=headers).json()
 
-# 종가 지난 200봉부터 시간 오름차순으로
+# 역시 시간 오름차순으로 받아옵니다
 trade_price = []
 for i in range(199, -1, -1):
     trade_price.append(response[i]['trade_price'])
@@ -36,19 +38,15 @@ df = pd.DataFrame(dic)
 means_A = df.loc[:,'price'].rolling(window=target_MA_1).mean()
 means_B = df.loc[:,'price'].rolling(window=target_MA_2).mean()
 
-# plt.plot(means_A[179:])
-# plt.plot(means_B[179:])
-# plt.show()
-
+# 추출한 MA는 가격값이기 때문에 학습에서 해줬던 것처럼 변화율로 바꿔줌
 ma_A = []
 ma_B = []
 for i in range(1, 200):
     ma_A.append((means_A[i]-means_A[i-1])/means_A[i-1] * 100)
     ma_B.append((means_B[i]-means_B[i-1])/means_B[i-1] * 100)
 
-# ma_A = ma_A[179:]
-# ma_B = ma_B[179:]
-
+# 모델에 넣기 위해 (20,2) window를 20개 생성해줍니다
+# 그렇다면 예측에 사용할 input은 (20,20,2)의 3중배열 형태겠죠
 inputs = []
 for i in range(19, -1, -1):
     window = []
@@ -56,15 +54,23 @@ for i in range(19, -1, -1):
         window.append([ma_A[179-i+j], ma_B[179-i+j]])
     inputs.append(window)
 
+# 모델에 넣으려면 numpy 배열로 바꿔줘야하기 때문에 변환해줍니다
 inputs = np.array(inputs)
 print(inputs)
 print(inputs.shape)
 
+# 모델에 넣어서 예측해줍니다
+# 마지막 노드 값을 출력하기 때문에 0~1의 실수값으로 나옵니다
+# 0.5 이상이면 1에 가깝기 때문에 상승을 예측한거고, 0.5 미만이 나오면 0에 가깝기 때문에 하락을 예측한 것이겠죠
 y_predict = model.predict(inputs)
 print(y_predict)
 
+# 모델 출력값이 0~1이기 때문에 백분율로 바꿔줍니다
+# 역시 50%이상이면 상승을 예측, 50 미만이면 하락을 예측
 percentage = round(float(y_predict[19]*100), 1)
 print("-----------------------------------------------------------------")
 print(targetTimeStr)
+# 모델 예측을 콘솔에 출력해줍니다
+# 근데 50% 미만이면 하락을 예측한다고 했으니까, 50% 미만이면 그냥 (100-percentage)해서 하락 기준의 확률로 표시해줬습니다
 print(str(target_MA_1)+","+str(target_MA_2)+"MA를 이용한 "+str(candles_data)+"분봉 다음 캔들 예상은 "+ (str(percentage) if percentage >=50 else str(100-percentage)) +"% 확률로 "+ ("상승" if percentage >= 50 else "하락")+ "합니다.")
-# print(y_predict)
+print(y_predict)
